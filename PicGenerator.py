67ceec760d85d3dc2c3c0ace0e346791d32f7c69
simonlegrand <slegrand@tartaglia.rocq.inria.fr>
2014-12-15 11:26:43 +0100

    Working PicGenerator

--- PicGenerator.py
+++ PicGenerator.py
@@ -9,6 +9,6 @@ img = np.ones((n,n))
 dims = np.shape(img)
 for i in range(dims[0]):
 	for j in range(dims[1]):
-		if i<(n/4) & j<(n/4):
-			img[i][j] = 1
+		if i<(n/4) and j<(n/4):
+			img[i][j] = 255
 scipy.misc.imsave('outfile.jpg', img)
